dbf631eda16a3c377278cbab1db4f3c334c2a0f3
Ben Shaughnessy <bs7280@gmail.com>
2024-03-20 13:32:59 -0500

    removed unused code

--- mistral_json_schema.py
+++ mistral_json_schema.py
@@ -1,14 +1,6 @@
 from llama_cpp.llama import Llama, LlamaGrammar
 import json
 
-if False:
-    import httpx
-    try:
-        1 / 0 
-        grammar_text = httpx.get("https://raw.githubusercontent.com/ggerganov/llama.cpp/master/grammars/json_arr.gbnf").text
-    except:
-        grammar_text = '# This is the same as json.gbnf but we restrict whitespaces at the end of the root array\n# Useful for generating JSON arrays\n\nroot   ::= arr\nvalue  ::= object | array | string | number | ("true" | "false" | "null") ws\n\narr  ::=\n  "[\\n" ws (\n            value\n    (",\\n" ws value)*\n  )? "]"\n\nobject ::=\n  "{" ws (\n            string ":" ws value\n    ("," ws string ":" ws value)*\n  )? "}" ws\n\narray  ::=\n  "[" ws (\n            value\n    ("," ws value)*\n  )? "]" ws\n\nstring ::=\n  "\\"" (\n    [^"\\\\] |\n    "\\\\" (["\\\\/bfnrt] | "u" [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F] [0-9a-fA-F]) # escapes\n  )* "\\"" ws\n\nnumber ::= ("-"? ([0-9] | [1-9] [0-9]*)) ("." [0-9]+)? ([eE] [-+]? [0-9]+)? ws\n\n# Optional space: by convention, applied in this grammar after literal chars when allowed\nws ::= ([ \\t\\n] ws)?\n'
-
 #grammar = LlamaGrammar.from_string(grammar_text)
 
 schema = '''
