c8a56d7a4a17c0896a513de7d95404cea5c70202
Ben Shaughnessy <bs7280@gmail.com>
2024-04-15 23:04:01 -0500

    added schedule example

--- schedule.py
+++ schedule.py
@@ -2,6 +2,7 @@ from llama_cpp.llama import Llama, LlamaGrammar
 import json
 from pydantic import BaseModel
 from datetime import datetime
+import time
 
 
 def get_grammar(schema: dict):
@@ -23,7 +24,25 @@ meeting_schema = MeetingSchedule.schema()
 grammar = get_grammar(meeting_schema)
 
 llm = Llama("mistral-7b-instruct-v0.1.Q4_K_M.gguf")
+
+st = time.time()
+
 prompt = "Lets catch up about the foobar project tomorrow at 4pm CST"
 response = llm(prompt, grammar=grammar, max_tokens=-1)
-print(f"Prompt: {prompt}")
+print(f"Elapsed: {time.time() - st}s Prompt: {prompt}")
 print(json.dumps(json.loads(response["choices"][0]["text"]), indent=4))
+
+print("------")
+
+print("------")
+
+st = time.time()
+
+prompt = "Generate a json object for the following meeting: Lets get lunch at Al's Beef tomorrow at lunch"
+response = llm(
+    prompt=prompt,
+    # grammar=grammar,
+    max_tokens=-1,
+)
+print(f"Elapsed: {time.time() - st}s Prompt: {prompt}")
+print(response["choices"][0]["text"])
